d72d8da4c23eb7b39fde7d007ddd2825c26f192b
amyymaa <amyyma@gmail.com>
2021-04-14 10:42:42 -0400

    update 04/14

--- codes/utils.py
+++ codes/utils.py
@@ -13,77 +13,26 @@ from skimage import io
 from PIL import Image
 from scipy.io import loadmat
 import random
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
 
 
-<<<<<<< HEAD
-class SpectrogramDataset(Dataset):
-    def __init__(self, file_path, val_dates, test_dates, mode='train', version='_Goose_1st', CH=None, upsample=False):
-        self.CH = CH
-        self.version = version
-        self.movement_files = os.listdir(file_path+'data'+version+'/move/')
-        self.sleeping_files = os.listdir(file_path+'data'+version+'/sleep/')
-        if upsample:
-            diff = len(self.sleeping_files)-len(self.movement_files)
-            try:
-                d = 0
-                while d < diff:
-                    ind = random.randint(0, len(self.movement_files)-1)
-                    x = self.movement_files[ind]
-                    x_date = x.split('_')[0]
-                    if x_date not in val_dates+test_dates:
-                        self.movement_files.append(x)
-                        d += 1
-            except ValueError:
-                print('Movoment instance more than sleep instances!')
-        all_files = self.sleeping_files + self.movement_files
-        if mode == 'train':
-            self.all_files = [f for f in all_files if f.split('_')[0] not in val_dates+test_dates]
-        elif mode == 'valid':
-            self.all_files = [f for f in all_files if f.split('_')[0] in val_dates]
-        elif mode == 'test':
-            self.all_files = [f for f in all_files if f.split('_')[0] in test_dates]
-=======
 class SpectrogramDatasetNew(Dataset):
     def __init__(self, files, load_path, CH=None):
         self.CH = CH
         self.files = files
         self.load_path = load_path
->>>>>>> 4109182083890cc0679eb0ffbd3f4d116935200d
   
     def __len__(self):
         return len(self.files)
     
     def __getitem__(self, idx):
-<<<<<<< HEAD
-        mvmt_type  = self.all_files[idx].split('_')[-1].split('.')[0]
-        date = self.all_files[idx].split('_')[0]
-        rec = self.all_files[idx].split('_')[1].split('_')[0]
-        time = float(self.all_files[idx].split('_')[3][4:])
-        path = file_path
-        spec = torch.from_numpy(np.load(path+'data'+self.version+'/'+ mvmt_type +'/' +self.all_files[idx])) 
-        if mvmt_type == 'move':
-            label = torch.Tensor([0])
-        elif mvmt_type == 'sleep':
-            label = torch.Tensor([1])
-        else:
-            label = torch.Tensor([-1])
-            
-=======
         f, label, mvmt_type, date, rec, time = self.files[idx]
         spec = torch.from_numpy(np.load(self.load_path+mvmt_type+'/'+f))
->>>>>>> 4109182083890cc0679eb0ffbd3f4d116935200d
         if self.CH is not None:
             return torch.transpose(spec[self.CH,:,:].unsqueeze(0), 2, 1), torch.Tensor([label]), date, rec, time
         else:
             return torch.transpose(spec, 2, 1), torch.Tensor([label]), date, rec, time
-
-<<<<<<< HEAD
-def create_dataloaders(val_dates, test_dates, version='v5', batch_size=32, CH=None, upsample=False):
-
-    train_dataset = SpectrogramDataset(val_dates=val_dates, test_dates=test_dates, mode='train', version=version, CH=CH, upsample=upsample)
-    valid_dataset = SpectrogramDataset(val_dates=val_dates, test_dates=test_dates, mode='valid', version=version, CH=CH, upsample=upsample)
-    test_dataset = SpectrogramDataset(val_dates=val_dates, test_dates=test_dates, mode='test', version=version, CH=CH, upsample=upsample)
-=======
         
 def create_files(load_path, val_dates, test_dates):
     train_files, val_files, test_files = [], [], []
@@ -127,7 +76,6 @@ def create_dataloaders(train_files, val_files, test_files, load_path, batch_size
     train_dataset = SpectrogramDatasetNew(files=train_files, load_path=load_path, CH=CH)
     valid_dataset = SpectrogramDatasetNew(files=val_files, load_path=load_path, CH=CH)
     test_dataset = SpectrogramDatasetNew(files=test_files, load_path=load_path, CH=CH)
->>>>>>> 4109182083890cc0679eb0ffbd3f4d116935200d
 
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle = True)
     val_loader = DataLoader(dataset=valid_dataset, batch_size=batch_size, shuffle = False)
@@ -162,7 +110,8 @@ def get_accuracy(model, loader, model_type='LR', collect_result=False, device='c
     with torch.no_grad():
         for data, labels, dates, recs, times in loader:
             data = data.to(device)
-            labels = labels.to(device).float()
+            # labels = labels.to(device).float()
+            labels = labels.to(device)
             outputs = model(data)
             predictions = get_pred(outputs, model_type=model_type)
             outputs = outputs.flatten().detach().cpu().numpy()
@@ -195,9 +144,8 @@ def get_accuracy(model, loader, model_type='LR', collect_result=False, device='c
         return accuracy, preds, preds_probs, labs, cases_wrong
     return accuracy
 
-<<<<<<< HEAD
 
-def get_loss(model, labels, outputs, alpha=0, loss_type='bce',timewindow = 10, reg_type='none', reduction='mean'):
+def get_loss(model, labels, outputs, alpha=0, timewindow=10, loss_type='bce', reg_type='none', reduction='mean'):
     if loss_type == 'hinge':
         labels[labels == 0] = -1
         if reduction == 'mean':
@@ -221,35 +169,7 @@ def get_loss(model, labels, outputs, alpha=0, loss_type='bce',timewindow = 10, r
     return loss
 
 
-def train(model, optimizer, loader, alpha, model_type='LR', loss_type='bce',timewindow = 10, reg_type=None, collect_result=False, device='cuda'):
-=======
-
-def get_loss(model, labels, outputs, alpha=0, loss_type='bce', reg_type='none', reduction='mean'):
-    if loss_type == 'hinge':
-        labels[labels == 0] = -1
-        if reduction == 'mean':
-            loss = torch.mean(torch.clamp(1 - labels*outputs, min=0))
-        elif reduction == 'sum':
-            loss = torch.sum(torch.clamp(1 - labels*outputs, min=0))
-    elif loss_type == 'bce':
-        if reduction == 'mean':
-            criterion = nn.BCELoss(reduction='mean')
-        elif reduction == 'sum':
-            criterion = nn.BCELoss(reduction='sum')
-        loss = criterion(torch.sigmoid(outputs), labels)
-    if reg_type != 'none':
-        weights = model.linear.weight.view(-1, 100, 10)
-    if reg_type == 'l2':
-        loss += alpha * weights.norm(2)
-    elif reg_type == 'finite_diff':
-        diff_h = (weights[:, :, 1:] - weights[:, :, :-1]).norm(2)
-        diff_v = (weights[:, 1:, :] - weights[:, :-1, :]).norm(2)
-        loss += alpha * (diff_h + diff_v)    
-    return loss
-
-
-def train(model, optimizer, loader, alpha, model_type='LR', loss_type='bce', reg_type=None, collect_result=False, device='cuda'):
->>>>>>> 4109182083890cc0679eb0ffbd3f4d116935200d
+def train(model, optimizer, loader, alpha, timewindow=10, model_type='LR', loss_type='bce', reg_type=None, collect_result=False, device='cuda'):
     model.train()
     batch_losses = 0
     batch_lengths = 0
@@ -263,13 +183,10 @@ def train(model, optimizer, loader, alpha, model_type='LR', loss_type='bce', reg
         
         outputs = model(data)
         outputs = outputs.reshape(outputs.shape[0],-1)
-<<<<<<< HEAD
-        loss = get_loss(model, labels, outputs, alpha=alpha, loss_type=loss_type, timewindow = timewindow, reg_type=reg_type, reduction='sum')
-=======
-        loss = get_loss(model, labels, outputs, alpha=alpha, loss_type=loss_type, reg_type=reg_type, reduction='sum')
->>>>>>> 4109182083890cc0679eb0ffbd3f4d116935200d
+        loss = get_loss(model, labels, outputs, alpha=alpha, timewindow=timewindow, loss_type=loss_type, reg_type=reg_type, reduction='sum')
         batch_losses += loss
         batch_lengths += labels.shape[0]
+        loss /= labels.shape[0]
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -283,11 +200,7 @@ def train(model, optimizer, loader, alpha, model_type='LR', loss_type='bce', reg
         acc = get_accuracy(model, loader, model_type=model_type, collect_result=False, device=device)
         return epoch_loss, acc
 
-<<<<<<< HEAD
-def evaluate(model, optimizer, loader, alpha, model_type='LR', loss_type='bce',timewindow = 10, reg_type=None, collect_result=False, device='cuda'):
-=======
-def evaluate(model, optimizer, loader, alpha, model_type='LR', loss_type='bce', reg_type=None, collect_result=False, device='cuda'):
->>>>>>> 4109182083890cc0679eb0ffbd3f4d116935200d
+def evaluate(model, optimizer, loader, alpha, timewindow=10, model_type='LR', loss_type='bce', reg_type=None, collect_result=False, device='cuda'):
     model.eval()
     batch_losses = 0
     batch_lengths = 0
@@ -302,11 +215,7 @@ def evaluate(model, optimizer, loader, alpha, model_type='LR', loss_type='bce', 
             
             outputs = model(data)
             outputs = outputs.reshape(outputs.shape[0],-1)
-<<<<<<< HEAD
-            loss = get_loss(model, labels, outputs, alpha=alpha, timewindow = timewindow,loss_type=loss_type, reg_type=reg_type, reduction='sum')
-=======
-            loss = get_loss(model, labels, outputs, alpha=alpha, loss_type=loss_type, reg_type=reg_type, reduction='sum')
->>>>>>> 4109182083890cc0679eb0ffbd3f4d116935200d
+            loss = get_loss(model, labels, outputs, alpha=alpha, timewindow=timewindow, loss_type=loss_type, reg_type=reg_type, reduction='sum')
             batch_losses += loss
             batch_lengths += labels.shape[0]
         
@@ -337,4 +246,74 @@ def plot_loss_acc(training_losses, val_losses, training_acc, validation_acc, mod
     plt.ylabel("Accuracy", fontsize = 15)
     plt.legend()
     plt.show()
+    
+
+def plot_weight_glm(device, path, model_type, CH, loss_type, reg_type, alpha, best_epoch, timewindow=10):
+    if CH == 'all':
+        model = GLM().to(device)
+        model.load_state_dict(torch.load('{}/{}_CH{}_LOSS{}_REG{}{}_EPOCH{}_REDUCEsum.pt'.format(path, model_type, CH, loss_type, reg_type, alpha, best_epoch)))
+        weights = model.linear.weight.view(62, 100, timewindow)
+        plt.figure(figsize=(15,8))
+        for i in range(62):
+            weights_sub = weights[i].detach().cpu()
+            plt.subplot(2,31,i+1)
+            if i==0 or i==31:
+                plt.yticks(ticks=[0, 20, 40, 60, 80, 99], labels=[round(np.logspace(0, 2.45, 100)[i]) for i in [0, 20, 40, 60, 80, 99]])
+                plt.xticks(ticks=[0, timewindow-1], labels=[1, timewindow])
+            else:
+                plt.axis('off')
+            plt.imshow(weights_sub)
+            plt.title(str(i+1))
+        plt.show()
+    else:
+        model = GLM(input_dim=100*timewindow).to(device)
+        model.load_state_dict(torch.load('{}/{}_CH{}_LOSS{}_REG{}{}_EPOCH{}_REDUCEsum.pt'.format(path, model_type, CH, loss_type, reg_type, alpha, best_epoch)))
+        weights = model.linear.weight.view(100, timewindow)
+        weights_sub = weights.detach().cpu()
+        plt.yticks(ticks=[0, 20, 40, 60, 80, 99], labels=[round(np.logspace(0, 2.45, 100)[i]) for i in [0, 20, 40, 60, 80, 99]])
+        plt.xticks(ticks=[0, timewindow-1], labels=[1, timewindow])
+        plt.imshow(weights_sub)
+        plt.title(str(CH))
+        plt.show()
+        
+def plot_confusion(test_preds, test_labels):
+    predictions_test = np.concatenate(test_preds)
+    labels_test = np.concatenate(test_labels)
+
+    cm_test = confusion_matrix(labels_test, predictions_test)
+    cm_test_percent = (cm_test.T/cm_test.astype(np.float).sum(axis=1)).T
+    plt.figure(figsize=(15, 5))
+    plt.subplot(1,2,1)
+    sn.heatmap(cm_test, annot = True,  fmt = 'd', cmap='Blues')
+    plt.title('Test Confusion Matrix')
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.subplot(1,2,2)
+    sn.heatmap(cm_test_percent, annot = True, cmap='Blues')
+    plt.title('Test Confusion Matrix (Rate)')
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.show()
+        
+        
+def tuning(train_loader, val_loader, model, optimizer, device, num_epochs, alpha, model_type, loss_type, reg_type, CH, path, timewindow=10, verbose=False):
+    training_losses, training_acc, val_losses, validation_acc = [], [], [], []
+    for epoch in range(num_epochs):
+        train_loss, train_acc = train(model, optimizer, train_loader, alpha=alpha, timewindow=timewindow, model_type=model_type, loss_type=loss_type, reg_type=reg_type, collect_result=False, device=device)
+        val_loss, val_acc = evaluate(model, optimizer, val_loader, alpha=alpha, timewindow=timewindow, model_type=model_type, loss_type=loss_type, reg_type=reg_type, collect_result=False, device=device)
+        training_losses.append(train_loss)
+        training_acc.append(train_acc)
+        val_losses.append(val_loss)
+        validation_acc.append(val_acc)
+        if val_loss <= min(val_losses):
+            best_epoch = epoch
+            print(epoch)
+            print('Train loss for epoch {}: {}'.format(epoch, train_loss))
+            print('Val loss for epoch {}: {}'.format(epoch, val_loss))
+            torch.save(model.state_dict(), '{}/{}_CH{}_LOSS{}_REG{}{}_EPOCH{}_REDUCEsum.pt'.format(path, model_type, CH, loss_type, reg_type, alpha, epoch))
+        elif verbose:
+            print('Train loss for epoch {}: {}'.format(epoch, train_loss))
+            print('Val loss for epoch {}: {}'.format(epoch, val_loss))
+    plot_loss_acc(training_losses, val_losses, training_acc, validation_acc, model_type)
+    plot_weight_glm(device, path, model_type, CH, loss_type, reg_type, alpha, best_epoch)
     
